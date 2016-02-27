6d4fe80615983cd8e598af95822f96dca3b71501
Wellingtoncezar <wellington-cezar@hotmail.com>
2016-02-27 14:34:29 -0300

    login - predefinição

--- sac/app/controllers/login.controller.php
+++ sac/app/controllers/login.controller.php
@@ -36,7 +36,52 @@ class login extends Controller{
 	* loagin no sistema
 	*/
 	public function logar(){
+		if(!isset($_POST['login']) || !isset($_POST['senha']))
+			return false;
+
+		$login = isset($_POST['login']) ? filter_var($_POST['login']) : '';
+		$senha = isset($_POST['senha']) ? filter_var($_POST['senha']) : '';
+		$captcha = isset($_POST['captcha']) ? filter_var($_POST['captcha']) : '';
 		
+		$this->load->model('loginModel');
+		$logon = new loginModel();
+		echo $logon->logar($login,$senha,$captcha);
 	}
+
+	/**
+	* Cria o captcha caso o número de tentativas de login falhos seja maior ou igual a 5
+	*/
+	public function captcha()
+	{
+		$captcha = new captcha();
+		if($_SESSION['ntentativaLogin'] >= 5)
+		{
+			$captcha->CreateImage();
+
+		}else
+			echo false;
+	}
+
+	/**
+	*verifica números de tentativa falhas de login
+	*/
+	public function verificaNTentativaLogin()
+	{
+		if($_SESSION['ntentativaLogin'] >= 5)
+			echo true;
+		else
+			echo false;
+	}
+
+
+	/**
+	* Encerra a sessão
+	*/
+	public function sair()
+	{
+		session_destroy();
+		header('Location: '.URL.'login');
+	}
+	
 
 }
