c4df6703029439f0f8ca7056caf070502b8d29d4
Wellingtoncezar <wellington-cezar@hotmail.com>
2016-09-12 00:34:45 -0300

    Alteração de privilégios do usuário 'gerencia' no banco

--- sac/banco de dados/privilegios.sql
+++ sac/banco de dados/privilegios.sql
@@ -9,25 +9,94 @@ GRANT SELECT, INSERT, UPDATE, REFERENCES ON `sac`.`fornecedores_agenda` TO 'func
 GRANT SELECT, INSERT, UPDATE, REFERENCES ON `sac`.`fornecedores` TO 'funcionarios'@'localhost';
 
 
+
+
+
+
 # Privileges for `gerencia`@`localhost`
 
 GRANT USAGE ON *.* TO 'gerencia'@'localhost' IDENTIFIED BY PASSWORD '*B2552112BB1980A174B3A6FC5C22280C0126CB0F';
 
-GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, TRIGGER ON `sac`.`cargos` TO 'gerencia'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`telefones_fornecedores` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`fornecedores_agenda` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`produtos` TO 'gerencia'@'localhost';
+
+GRANT SELECT ON `sac`.`sys_paginas` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`orcamentos` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`checkout` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`categorias` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`emails` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`requisicao_usuario` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`funcionarios` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`estoque` TO 'gerencia'@'localhost';
 
 GRANT SELECT ON `sac`.`nivel_acesso` TO 'gerencia'@'localhost';
 
-GRANT SELECT ON `sac`.`sys_modulos` TO 'gerencia'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`enderecos` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`fornecedores` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`abertura_caixa` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`telefones_funcionarios` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`orcamento_produto` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`unidade_medida` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`requisicoes` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`cargos` TO 'gerencia'@'localhost';
 
 GRANT SELECT, UPDATE ON `sac`.`sys_usuarios` TO 'gerencia'@'localhost';
 
-GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, TRIGGER ON `sac`.`funcionarios` TO 'gerencia'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`produto_fornecedores` TO 'gerencia'@'localhost';
 
 GRANT SELECT ON `sac`.`sys_actions` TO 'gerencia'@'localhost';
 
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`telefones` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`requisicao_produto` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`emails_fornecedores` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`produto_lote` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`fornecedores_agenda_notificado` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`enderecos_fornecedores` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`enderecos_funcionarios` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`marcas` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`emails_funcionarios` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`vendas` TO 'gerencia'@'localhost';
+
+GRANT SELECT ON `sac`.`sys_modulos` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`unidade_medida_produto` TO 'gerencia'@'localhost';
+
 GRANT SELECT, INSERT, UPDATE ON `sac`.`sys_usuarios_acessos` TO 'gerencia'@'localhost';
 
-GRANT SELECT ON `sac`.`sys_paginas` TO 'gerencia'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`cotacoes` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`produtos_vendidos` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`localizacao_lote` TO 'gerencia'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES, INDEX, TRIGGER ON `sac`.`nivel_estoque` TO 'gerencia'@'localhost';
+
 
 
 # Privileges for `userlogin`@`localhost`
